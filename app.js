80070510830bf3517115d0e116db67d61df60a2e
BrianLoughnane <BrianJLoughnane@gmail.com>
2014-10-07 19:45:10 -0400

    Full Functionality

--- app.js
+++ app.js
@@ -9,11 +9,9 @@ angular.module('myApp',[])
 		$scope.taxRate = 0;
 		$scope.tipRate = 0;
 
-		$scope.taxRatePercentage = ($scope.taxRate / 100);
-		$scope.subtotal = $scope.basePrice + ($scope.basePrice * $scope.taxRatePercentage);
-		$scope.tipRatePercentage = $scope.tipRate/100;
-		$scope.tip = $scope.basePrice * $scope.tipRatePercentage;
-		$scope.total = $scope.subtotal + $scope.tip;	
+		$scope.subtotal = 0;
+		$scope.tip = 0;
+		$scope.total = 0;	
 
 		$scope.$watch('basePrice', function() {
 			$scope.taxRatePercentage = ($scope.taxRate / 100);
@@ -36,10 +34,41 @@ angular.module('myApp',[])
 		});
 
 
-		$scope.myEarnings = {
-			tipTotal: 0,
-			mealCount: 0,
+		// $scope.myEarnings = {
+		// 	tipTotal: 0,
+		// 	mealCount: 0,
+		// };
+
+		$scope.tipTotal = 0;
+		$scope.mealCount = 0;
+		$scope.averageTip = 0;
+
+		$scope.$watch('tipTotal', function() {
+			$scope.averageTip = $scope.tipTotal / $scope.mealCount;		
+		});
+		
+		$scope.$watch('mealCount', function() {
+			$scope.averageTip = $scope.tipTotal / $scope.mealCount;		
+		});
+
+		$scope.submit = function() {
+			if($scope.myForm.$valid) {
+				$scope.mealCount += 1;
+				$scope.tipTotal += $scope.tip;
+			}
 		};
 
-		$scope.averageTip = $scope.myEarnings.tipTotal / $scope.myEarnings.mealCount;
+		$scope.cancel = function() {
+			$scope.basePrice = 0;
+			$scope.taxRate = 0;
+			$scope.tipRate = 0;
+		};
+
+		$scope.reset = function() {
+			$scope.cancel();
+			$scope.tipTotal = 0;
+			$scope.mealCount = 0;
+			$scope.averageTip = 0;
+		};
+
 	});
