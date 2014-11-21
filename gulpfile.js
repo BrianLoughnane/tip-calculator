15496333bbb1fdf5e9c85470eac8064c3f5d46a0
BrianLoughnane <BrianJLoughnane@gmail.com>
2014-11-21 15:04:17 -0500

    added some files

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+//gulpfile.js
+
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+
+gulp.task('connect', function() {
+	connect.server({
+		root: 'app/'
+	});
+});
+
+gulp.task('default', ['connect']);
